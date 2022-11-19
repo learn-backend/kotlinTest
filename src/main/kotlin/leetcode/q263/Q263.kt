726258191d79d1e46afa3191fe0346af3d27863a
giyeon95 <giyeon15@gmail.com>
2022-11-20 01:33:42 +0900

    leetCode: 263

---
+++ src/main/kotlin/leetcode/q263/Q263.kt
@@ -0,0 +1,26 @@
+package leetcode.q263
+
+class Q263 {
+
+    fun isUgly(n: Int): Boolean {
+        if (n <= 0) {
+            return false
+        }
+
+        var result = n
+
+        while (result > 1) {
+            result /= if (result % 2 == 0) {
+                2
+            } else if (result % 3 == 0) {
+                3
+            } else if (result % 5 == 0) {
+                5
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+}
