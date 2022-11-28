b90adbbc8e488fe847f559a089c812ed5c3ff381
giyeon95 <giyeon15@gmail.com>
2022-11-28 18:13:43 +0900

    feat: 907 1차 풀이 (81 / 87 testcases passed)

--- src/main/kotlin/leetcode/q907/Q907.kt
+++ src/main/kotlin/leetcode/q907/Q907.kt
@@ -4,3 +4,52 @@ package leetcode.q907
 
 class Q907 {
 }
+
+
+fun main() {
+    val sumSubarrayMins = sumSubarrayMins(intArrayOf(11, 81, 94, 43, 3))
+    println(sumSubarrayMins)
+}
+
+val M = 1_000_000_007
+fun sumSubarrayMins(arr: IntArray): Int {
+    val n = arr.size
+
+    val d = IntArray(n)
+
+    if (arr.isEmpty()) {
+        return 0
+    }
+
+    if (arr.size == 1) {
+        return arr.single()
+    }
+
+    d[0] = arr[0]
+    d[1] = d[0] + arr[1] + Math.min(arr[0], arr[1])
+
+    for (i in 2 until n) {
+        d[i] = d[i - 1] + minSumValue(arr, i)
+
+        if (d[i] >= M) {
+            d[i] %= M
+        }
+    }
+
+    return d[n - 1]
+}
+
+// 0 -> arr[0]
+// 1 -> min(arr(1), arr(0)), min(arr(1))
+// 2 -> min(arr(2), arr(1), arr(0)) + min(arr(2), arr(1)) + min(arr(2))
+fun minSumValue(arr: IntArray, idx: Int): Int {
+    val d = IntArray(idx + 1)
+
+    d[0] = arr[idx]
+
+    for (i in 1..idx) {
+        d[i] = Math.min(d[i - 1], arr[idx - i])
+    }
+
+    return d.sum()
+}
