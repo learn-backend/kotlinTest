174ed1b747e81a0bae3a0d37919dac83374c52de
rhkddud3917 <biggwang@gmail.com>
2022-12-12 18:49:18 +0900

    leetcode 1342

--- src/main/kotlin/leetcode/q1342/Q1342.kt
+++ src/main/kotlin/leetcode/q1342/Q1342.kt
@@ -1,5 +1,6 @@
 package leetcode.q1342
 
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
 class Solution {
     fun numberOfSteps(num: Int): Int {
         var num1 = num
